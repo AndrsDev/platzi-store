90ea964bdba626e5066de5b69f2071d5b2dfd9e1
Andres Sanabria <andrs.dev@gmail.com>
2020-12-26 09:45:32 -0600

    added missing test case

--- src/__test__/utils/getData.test.js
+++ src/__test__/utils/getData.test.js
@@ -9,6 +9,7 @@ describe('Fetch API', () => {
     fetch.mockResponseOnce(JSON.stringify({ data: '12345' }));
     const response = await getData('https://google.com');
     expect(response.data).toEqual('12345');
+    expect(fetch.mock.calls[0][0]).toEqual('https://google.com');
   });
 
 });
